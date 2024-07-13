491889d44e226e57023f619a87b7b61833101ad6
rinzio <>
2024-07-12 20:25:23 -0600

    Url blocking

---
+++ src/block.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+
+	urls := os.Args[1:]
+	file, err := os.OpenFile("/etc/hosts", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		fmt.Println("Error opening file:", err)
+		return
+	}
+	defer file.Close()
+
+	// Create a new writer
+	writer := bufio.NewWriter(file)
+	for _, line := range urls {
+		fmt.Println("Blocking " + line + "...")
+		_, err := writer.WriteString("127.0.0.1 " + line + "\n")
+		if err != nil {
+			fmt.Println("Error writing to file:", err)
+			return
+		}
+	}
+
+	// Flush the writer
+	err = writer.Flush()
+	if err != nil {
+		fmt.Println("Error flushing the writer:", err)
+		return
+	}
+
+	fmt.Println("Lines written successfully!")
+}
